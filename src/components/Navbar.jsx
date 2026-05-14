077e85c1c6548abc10daae82e885ed9ac89fc18c
princethakur19 <princethakur7778@gmail.com>
2026-05-14 13:04:51 +0530

    done for today

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,27 @@ function Navbar() {
 
         {/* Nav Links */}
         <ul className="nav-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Projects</a></li>
-          <li><a href="#">Skills</a></li>
-          <li><a href="#">Contact</a></li>
+
+          <li>
+            <a href="#home">Home</a>
+          </li>
+
+          <li>
+            <a href="#about">About</a>
+          </li>
+
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+
         </ul>
 
         {/* Resume Button */}

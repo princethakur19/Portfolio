b4b65ffb413359435d8566c0fa6ded2eaefed90d
princethakur19 <princethakur7778@gmail.com>
2026-05-20 10:53:24 +0530

    update in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About"
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }

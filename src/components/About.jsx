db9664928c057df751797ebb9e3a1981d8ddb4b3
princethakur19 <princethakur7778@gmail.com>
2026-07-12 23:23:13 +0530

    push commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -86,7 +86,7 @@ function About() {
             download="Prince_Thakur_Resume.pdf"
             className="download-btn"
           >
-            Download
+            Download CV
           </a>
         </div>
       </div>
